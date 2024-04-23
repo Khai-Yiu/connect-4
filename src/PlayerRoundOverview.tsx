d22f5c1faf683b0ce90d56241bf75743efed1abf
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-04-23 15:50:15 +0800

    PlayerRoundOverview

---
+++ src/PlayerRoundOverview.tsx
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+export type PlayerRoundOverviewProps = {
+    player?: 1 | 2;
+    remainingTokens: number;
+    isActiveTurn: boolean;
+    tokenColour: string;
+};
+
+const StyledWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: blue;
+    color: white;
+    font-size: 1.2rem;
+    padding: 10px 10px;
+    max-width: 50%;
+`;
+
+const StyledRow = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const StyledToken = styled.div<{ isActiveTurn: boolean; tokenColour: string }>`
+    background-color: ${(props) =>
+        props.isActiveTurn ? props.tokenColour : 'initial'};
+    border: ${(props) => (props.isActiveTurn ? '3px white dotted' : 'none')};
+    border-radius: 50%;
+    height: 50px;
+    width: 50px;
+    margin-left: 20px;
+`;
+
+export const PlayerRoundOverview = ({
+    player,
+    remainingTokens,
+    isActiveTurn,
+    tokenColour
+}: PlayerRoundOverviewProps) => (
+    <StyledWrapper>
+        <StyledRow>
+            <p>{`Player number: ${player}`}</p>
+            <StyledToken
+                isActiveTurn={isActiveTurn}
+                tokenColour={tokenColour}
+            />
+        </StyledRow>
+        <p>{`Remaining tokens: ${remainingTokens}`}</p>
+    </StyledWrapper>
+);
