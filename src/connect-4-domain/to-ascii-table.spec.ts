157f87e5ac12ad15c7f09589d93f6317bcefd10d
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-05-07 16:11:31 +0800

    RED: to-ascii-table > given a one row grid > with one column > containing null > returns a 1x1 ascii table

--- src/connect-4-domain/to-ascii-table.spec.ts
+++ src/connect-4-domain/to-ascii-table.spec.ts
@@ -9,20 +9,51 @@ describe('to-ascii-table', () => {
     });
     describe('given a one row grid', () => {
         describe('with one column', () => {
-            it('returns a 1x1 ascii table', () => {
-                const asciiTable = toAsciiTable([['1']]);
-                expect(asciiTable).toEqual(`
+            describe('containing a string', () => {
+                describe('and the string is empty', () => {
+                    it('returns a 1x1 ascii table', () => {
+                        const asciiTable = toAsciiTable([['']]);
+                        expect(asciiTable).toEqual(`
+|--|
+|  |
+|--|`);
+                    });
+                });
+                describe('with content 1 character in length', () => {
+                    it('returns a 1x1 ascii table', () => {
+                        const asciiTable = toAsciiTable([['1']]);
+                        expect(asciiTable).toEqual(`
 |---|
 | 1 |
 |---|`);
-            });
-            describe('with content greater than 1 character in length', () => {
-                it('returns a 1x1 ascii table', () => {
-                    const asciiTable = toAsciiTable([['10']]);
-                    expect(asciiTable).toEqual(`
+                    });
+                });
+                describe('with content greater than 1 character in length', () => {
+                    it('returns a 1x1 ascii table', () => {
+                        const asciiTable = toAsciiTable([['10']]);
+                        expect(asciiTable).toEqual(`
 |----|
 | 10 |
 |----|`);
+                    });
+                });
+            });
+            describe("containing 'undefined'", () => {
+                it('returns a 1x1 ascii table', () => {
+                    const asciiTable = toAsciiTable([[undefined]]);
+                    expect(asciiTable).toEqual(`
+|--|
+|  |
+|--|`);
+                });
+            });
+            describe('containing null', () => {
+                it('returns a 1x1 ascii table', () => {
+                    const asciiTable = toAsciiTable([[null]]);
+                    expect(asciiTable).toEqual(`
+|--|
+|  |
+|--|`);
                 });
             });
         });
--- src/connect-4-domain/to-ascii-table.ts
+++ src/connect-4-domain/to-ascii-table.ts
@@ -2,22 +2,26 @@ function createBorder(cellWidth: number): string {
     return `|${'-'.repeat(cellWidth + 2)}|`;
 }
 
-function toAsciiTable(grid: Array<Array<string>>): string {
+function toAsciiTable(grid: Array<Array<string | undefined>>): string {
     if (grid.length === 0) {
         return '';
     }
 
     const tableRows = grid.reduce((tableRows, row) => {
         tableRows.push(
-            row.reduce((rowContent, element) => {
-                return rowContent.concat(`| ${element} |`);
+            row.reduce((rowContent: string, element) => {
+                return element !== undefined
+                    ? rowContent.concat(`| ${element} |`)
+                    : '|  |';
             }, '')
         );
 
         return tableRows;
     }, []);
 
-    const border = createBorder(grid[0][0].length);
+    const border = createBorder(
+        grid[0][0] !== undefined ? grid[0][0].length : 0
+    );
     return ['', border, tableRows[0], border].join('\n');
 }
 
