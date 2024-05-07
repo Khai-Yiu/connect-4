6553dee9d2b6acb419c297a0032bbe3beffd1eec
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-05-07 16:11:41 +0800

    GREEN: to-ascii-table > given a one row grid > with one column > containing null > returns a 1x1 ascii table

--- src/connect-4-domain/to-ascii-table.ts
+++ src/connect-4-domain/to-ascii-table.ts
@@ -2,7 +2,7 @@ function createBorder(cellWidth: number): string {
     return `|${'-'.repeat(cellWidth + 2)}|`;
 }
 
-function toAsciiTable(grid: Array<Array<string | undefined>>): string {
+function toAsciiTable(grid: Array<Array<string | undefined | null>>): string {
     if (grid.length === 0) {
         return '';
     }
@@ -10,7 +10,7 @@ function toAsciiTable(grid: Array<Array<string | undefined>>): string {
     const tableRows = grid.reduce((tableRows, row) => {
         tableRows.push(
             row.reduce((rowContent: string, element) => {
-                return element !== undefined
+                return element !== undefined && element !== null
                     ? rowContent.concat(`| ${element} |`)
                     : '|  |';
             }, '')
@@ -20,7 +20,7 @@ function toAsciiTable(grid: Array<Array<string | undefined>>): string {
     }, []);
 
     const border = createBorder(
-        grid[0][0] !== undefined ? grid[0][0].length : 0
+        grid[0][0] !== undefined && grid[0][0] !== null ? grid[0][0].length : 0
     );
     return ['', border, tableRows[0], border].join('\n');
 }
