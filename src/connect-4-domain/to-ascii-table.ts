081c519836f6de5ddc81f886dd4487e2c55d21f0
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-05-10 14:42:34 +0800

    GREEN: to-ascii-table > given a one row grid > with one column > and uses a custom cell resolver > uses the custom cell resolver to resolve the cell's value

--- src/connect-4-domain/to-ascii-table.ts
+++ src/connect-4-domain/to-ascii-table.ts
@@ -2,26 +2,40 @@ function createBorder(cellWidth: number): string {
     return `|${'-'.repeat(cellWidth + 2)}|`;
 }
 
-function toAsciiTable(grid: Array<Array<string | undefined | null>>): string {
+function defaultResolver<T>(value: T) {
+    if (value === null || value === undefined) {
+        return '';
+    }
+
+    return `${value}`;
+}
+
+function toAsciiTable<T>(
+    grid: Array<Array<T>>,
+    cellResolver: (value: any) => string = defaultResolver
+): string {
     if (grid.length === 0) {
         return '';
     }
 
+    let cellWidth = 0;
     const tableRows = grid.reduce((tableRows, row) => {
         tableRows.push(
-            row.reduce((rowContent: string, element) => {
-                return element !== undefined && element !== null
-                    ? rowContent.concat(`| ${element} |`)
-                    : '|  |';
+            row.reduce((rowContent, currentElement) => {
+                const resolvedElement = cellResolver(currentElement);
+                cellWidth =
+                    resolvedElement.length > cellWidth
+                        ? resolvedElement.length
+                        : cellWidth;
+
+                return rowContent.concat(`| ${resolvedElement} |`);
             }, '')
         );
 
         return tableRows;
-    }, []);
+    }, [] as Array<string>);
 
-    const border = createBorder(
-        grid[0][0] !== undefined && grid[0][0] !== null ? grid[0][0].length : 0
-    );
+    const border = createBorder(cellWidth);
     return ['', border, tableRows[0], border].join('\n');
 }
 
