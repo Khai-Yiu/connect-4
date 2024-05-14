1b416c88fe1d5760b7a0c79753993558bd4a335a
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-05-14 16:28:44 +0800

    GREEN: toBeDeeplyUnequal > should fail when given objects are the same object

---
+++ src/connect-4-domain/to-be-deeply-unequal.ts
@@ -0,0 +1,14 @@
+import { MatcherResult } from '@/vitest';
+
+function toBeDeeplyUnequal(
+    this: { isNot: boolean } | void,
+    received: object,
+    expected: object
+): MatcherResult {
+    return {
+        pass: received !== expected,
+        message: () => `Objects are deeply ${isNot ? 'un' : ''}equal`
+    };
+}
+
+export default toBeDeeplyUnequal;
