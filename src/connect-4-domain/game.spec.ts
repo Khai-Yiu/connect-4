539020cac988fb3d9b7567f1b68935a0ca061608
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-05-14 15:01:13 +0800

    RED: game > new game > given defaults ? creates a game where player 1 starts with half the tokens of the number of cells

--- src/connect-4-domain/game.spec.ts
+++ src/connect-4-domain/game.spec.ts
@@ -1,5 +1,14 @@
 import { describe, it, expect } from 'vitest';
 import GameFactory from '@/connect-4-domain/game';
+import _toAsciiTable from '@/connect-4-domain/to-ascii-table';
+import { BoardCell } from '@/connect-4-domain/game';
+
+function toAsciiTable(board: Array<Array<BoardCell>>): string {
+    const cellResolver = (cell: BoardCell) =>
+        cell.player === undefined ? '' : `${cell.player}`;
+
+    return _toAsciiTable(board, cellResolver);
+}
 
 describe('game', () => {
     describe('new game', () => {
@@ -11,148 +20,32 @@ describe('game', () => {
             it('creates a 6x7 board', () => {
                 const game = new GameFactory();
                 const board = game.getBoard();
+                const asciiBoard = toAsciiTable(board);
                 expect(asciiBoard).toMatchInlineSnapshot(`
-                  [
-                    [
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                    ],
-                    [
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                    ],
-                    [
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                    ],
-                    [
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                    ],
-                    [
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                    ],
-                    [
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                      {
-                        "player": undefined,
-                      },
-                    ],
-                  ]
+                  "
+                  |--|--|--|--|--|--|--|
+                  |  |  |  |  |  |  |  |
+                  |--|--|--|--|--|--|--|
+                  |  |  |  |  |  |  |  |
+                  |--|--|--|--|--|--|--|
+                  |  |  |  |  |  |  |  |
+                  |--|--|--|--|--|--|--|
+                  |  |  |  |  |  |  |  |
+                  |--|--|--|--|--|--|--|
+                  |  |  |  |  |  |  |  |
+                  |--|--|--|--|--|--|--|
+                  |  |  |  |  |  |  |  |
+                  |--|--|--|--|--|--|--|"
                 `);
+            });
+            it(`creates a game where player 1 starts with half the tokens of the number of cells`, () => {
+                const game = new GameFactory();
+                expect(game.getPlayerStats(1)).toEqual(
+                    expect.objectContaining({
+                        playerNumber: 1,
+                        remainingDiscs: 21
+                    })
+                );
             });
         });
     });
