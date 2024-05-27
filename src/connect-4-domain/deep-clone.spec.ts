f49f27e822ada0b2fc40caa5d27a71276127cc18
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-05-27 16:27:09 +0800

    GREEN: deepClone > should return a primitive value as is

--- src/connect-4-domain/deep-clone.spec.ts
+++ src/connect-4-domain/deep-clone.spec.ts
@@ -1,4 +1,5 @@
 import { describe, it, expect } from 'vitest';
+import deepClone from '@/connect-4-domain/deep-clone';
 
 describe('deepClone', () => {
     it('should return a primitive value as is', () => {
