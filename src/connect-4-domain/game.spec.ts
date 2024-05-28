46340e0ded822f6c6c664405a19aaa6c54c8b7b4
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-05-28 15:37:11 +0800

    GREEN: game > new game > given custom board dimensions > with negative number of rows

--- src/connect-4-domain/game.spec.ts
+++ src/connect-4-domain/game.spec.ts
@@ -112,6 +112,20 @@ describe('game', () => {
                     );
                 });
             });
+            describe('with negative number of columnss', () => {
+                it('throws an error', () => {
+                    expect(
+                        () =>
+                            new GameFactory({
+                                boardDimensions: { rows: -1, columns: 7 }
+                            })
+                    ).toThrowError(
+                        new InvalidBoardDimensionsError(
+                            'Number of rows must be greater than or equal to 1'
+                        )
+                    );
+                });
+            });
             describe('which result in an odd number of cells', () => {
                 it('throws an error', () => {
                     expect(
