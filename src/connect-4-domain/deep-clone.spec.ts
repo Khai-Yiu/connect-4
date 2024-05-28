0b14660a79d6169b20457d2bc60de87155cadca7
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-05-28 13:36:53 +0800

    RED/GREEN: deepClone > should return a deep copy of an object

--- src/connect-4-domain/deep-clone.spec.ts
+++ src/connect-4-domain/deep-clone.spec.ts
@@ -6,17 +6,37 @@ describe('deepClone', () => {
         expect(deepClone(2)).toBe(2);
     });
     it('should return a deep copy of an array', () => {
-        const original: (object | number[] | number)[] = [{ a: 1 }, 2, [3, 4]];
+        const original: [{ a: number }, number, number[]] = [
+            { a: 1 },
+            2,
+            [3, 4]
+        ];
         const cloned = deepClone(original);
         expect(cloned).not.toBe(original);
         expect(cloned[0]).not.toBe(original[0]);
         expect(cloned[0]).toBeInstanceOf(Object);
         expect(Object.keys(cloned[0])).toHaveLength(1);
         expect(Object.hasOwn(cloned[0], 'a')).toBeTruthy();
-        expect(cloned[0]['a']).toStrictEqual(original[0]['a']);
+        expect(cloned[0].a).toStrictEqual(original[0].a);
         expect(cloned[1]).toBe(original[1]);
         expect(cloned[2]).not.toBe(original[2]);
         expect(cloned[2][0]).toBe(original[2][0]);
         expect(cloned[2][1]).toBe(original[2][1]);
+    });
+    it('should return a deep copy of an object', () => {
+        const original: {
+            a: number;
+            b: { c: string; d: number };
+            e: number[];
+        } = { a: 42, b: { c: 'Hi', d: 9 }, e: [3, 4] };
+        const cloned = deepClone(original);
+        expect(cloned).not.toBe(original);
+        expect(cloned.a).toStrictEqual(original.a);
+        expect(cloned.b).not.toBe(original.b);
+        expect(cloned.b.c).toStrictEqual(original.b.c);
+        expect(cloned.b.d).toStrictEqual(original.b.d);
+        expect(cloned.e).not.toBe(original.e);
+        expect(cloned.e[0]).toStrictEqual(original.e[0]);
+        expect(cloned.e[1]).toStrictEqual(original.e[1]);
     });
 });
