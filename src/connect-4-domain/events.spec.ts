badab7e51e2a26eeb487405ce5ac601334d11bd2
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-05-30 14:41:03 +0800

    RED: events > createPlayerMoveFailedEvent > returns a PlayerMovedFailEvent

---
+++ src/connect-4-domain/events.spec.ts
@@ -0,0 +1,26 @@
+import { describe, it, expect } from 'vitest';
+import {
+    createPlayerMoveFailedEvent,
+    PlayerMoveFailedEvent
+} from '@/connect-4-domain/events';
+
+describe('events', () => {
+    describe('createPlayerMoveFailedEvent', () => {
+        it('returns a PlayerMovedFailedEvent', () => {
+            const playerMovedFailedEvent = createPlayerMoveFailedEvent({
+                message:
+                    "Cell at row -1 and column 0 doesn't exist on the board. The row number must be >= 1 and <= 1"
+            });
+            expect(playerMovedFailedEvent).toBeInstanceOf(
+                PlayerMoveFailedEvent
+            );
+            expect(playerMovedFailedEvent).toEqual({
+                type: 'PLAYER_MOVED_FAILED',
+                payload: {
+                    message:
+                        "Cell at row -1 and column 0 doesn't exist on the board. The row number must be >= 1 and <= 1"
+                }
+            });
+        });
+    });
+});
