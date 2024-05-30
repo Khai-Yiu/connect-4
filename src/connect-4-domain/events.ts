0c57631acabcb877e255eacad18622e8293efb4c
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-05-30 16:05:10 +0800

    GREEN: events > createPlayerMovedEvent > returns a PlayerMovedEvent

--- src/connect-4-domain/events.ts
+++ src/connect-4-domain/events.ts
@@ -2,8 +2,17 @@ type PlayerMovedFailedEventPayload = {
     message: string;
 };
 
+type PlayerMovedEventPayload = {
+    player: 1 | 2;
+    targetCell: {
+        row: number;
+        column: number;
+    };
+};
+
 enum EventTypes {
-    PLAYER_MOVE_FAILED = 'PLAYER_MOVE_FAILED'
+    PLAYER_MOVE_FAILED = 'PLAYER_MOVE_FAILED',
+    PLAYER_MOVED = 'PLAYER_MOVED'
 }
 
 export function createPlayerMoveFailedEvent(
@@ -18,6 +27,22 @@ export class PlayerMoveFailedEvent {
 
     constructor(eventPayload: PlayerMovedFailedEventPayload) {
         this.type = EventTypes.PLAYER_MOVE_FAILED;
+        this.payload = eventPayload;
+    }
+}
+
+export function createPlayerMovedEvent(
+    eventPayload: PlayerMovedEventPayload
+): PlayerMovedEvent {
+    return new PlayerMovedEvent(eventPayload);
+}
+
+export class PlayerMovedEvent {
+    type: string;
+    payload: PlayerMovedEventPayload;
+
+    constructor(eventPayload: PlayerMovedEventPayload) {
+        this.type = EventTypes.PLAYER_MOVED;
         this.payload = eventPayload;
     }
 }
