5b5a337c259a27569b286c3cce276ff86f869cde
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-06-04 15:35:32 +0800

    GREEN: parse-ascii-table > given a 1x1 ascii table > with a non-empty cell > and a customer cell resolver > returns a 1x1 grid with a resolved value

--- src/connect-4-domain/parse-ascii-table.ts
+++ src/connect-4-domain/parse-ascii-table.ts
@@ -1,4 +1,13 @@
-function parseAsciiTable(asciiTable: string): Array<Array<undefined | string>> {
+function defaultCustomResolver(value: string): string | undefined {
+    return value.length === 0 ? undefined : value;
+}
+
+function parseAsciiTable<T>(
+    asciiTable: string,
+    customResolver: (value: string) => T = defaultCustomResolver as (
+        value: string
+    ) => T
+): Array<Array<T>> {
     if (asciiTable.length === 0) {
         return [];
     }
@@ -6,21 +15,17 @@ function parseAsciiTable(asciiTable: string): Array<Array<undefined | string>> {
     const asciiTableRows = asciiTable.split('\n').slice(1);
     const grid = asciiTableRows.reduce(
         (
-            grid: Array<Array<undefined | string>>,
+            grid: Array<Array<T>>,
             row: string,
             currentIndex: number
-        ): Array<Array<undefined | string>> => {
+        ): Array<Array<T>> => {
             if (currentIndex % 2 == 0) {
                 return grid;
             }
 
             const rowCells = row.split('|');
 
-            if (rowCells[1].trim().length === 0) {
-                grid.push([undefined]);
-            } else {
-                grid.push([rowCells[1].trim()]);
-            }
+            grid.push([customResolver(rowCells[1].trim()) as T]);
 
             return [...grid];
         },
