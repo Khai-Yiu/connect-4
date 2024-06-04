060c66cc1025940f2117d18928b2b42cc6ecbdec
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-06-04 15:14:44 +0800

    GREEN: parse-ascii-table > given a 1x1 ascii table > returns a 1x1 grid with an empty cell

--- src/connect-4-domain/parse-ascii-table.ts
+++ src/connect-4-domain/parse-ascii-table.ts
@@ -1,5 +1,31 @@
-function parseAsciiTable(asciiTable: string): [] {
-    return asciiTable ? [] : [];
+function parseAsciiTable<T>(asciiTable: string): Array<Array<T>> {
+    if (asciiTable.length === 0) {
+        return [];
+    }
+
+    const asciiTableRows = asciiTable.split('\n').slice(1);
+    const grid = asciiTableRows.reduce(
+        (
+            grid: Array<Array<T>>,
+            row: string,
+            currentIndex: number
+        ): Array<Array<T>> => {
+            if (currentIndex % 2 == 0) {
+                return grid;
+            }
+
+            const rowCells = row.split('|');
+
+            if (rowCells[1].trim().length === 0) {
+                grid.push([undefined as T]);
+            }
+
+            return [...grid];
+        },
+        []
+    );
+
+    return grid;
 }
 
 export default parseAsciiTable;
