91016a7e1c6993ec0d908146e60c0a430aa7c4c8
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-06-17 16:45:36 +0800

    GREEN: in-memory-repository > given defaults > saves a board

--- src/connect-4-domain/in-memory-repository.ts
+++ src/connect-4-domain/in-memory-repository.ts
@@ -1,3 +1,24 @@
-interface GameRepositoryInterface {}
+import { Board } from '@/connect-4-domain/game';
 
-export default class InMemoryRepository implements GameRepositoryInterface {}
+type BoardUuid = `${string}-${string}-${string}-${string}-${string}`;
+
+type Store = Map<BoardUuid, Board>;
+
+interface GameRepositoryInterface {
+    save: (board: Board) => BoardUuid;
+}
+
+export default class InMemoryRepository implements GameRepositoryInterface {
+    private store: Map<BoardUuid, Board>;
+
+    constructor(store: Store = new Map<BoardUuid, Board>()) {
+        this.store = store;
+    }
+
+    save(board: Board) {
+        const boardUuid = crypto.randomUUID();
+        this.store.set(boardUuid, board);
+
+        return boardUuid;
+    }
+}
