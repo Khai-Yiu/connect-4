b2971e5b046dcfaa0fcb72d1658f9b9d33507dda
Khai-Yiu Soh <khai.soh@journeyone.com.au>
2024-06-17 16:25:02 +0800

    RED: in-memory-repository > given a store > saves a board

--- src/connect-4-domain/in-memory-repository.spec.ts
+++ src/connect-4-domain/in-memory-repository.spec.ts
@@ -1,11 +1,41 @@
+import { BoardCell } from '@/connect-4-domain/game';
 import InMemoryRepository from '@/connect-4-domain/in-memory-repository';
+import parseAsciiTable from '@/connect-4-domain/parse-ascii-table';
 import { describe, expect, it } from 'vitest';
 
 describe('in-memory-repository', () => {
+    const customResolver = (value: string): BoardCell => {
+        const parsedValue = Number.parseInt(value);
+
+        if (parsedValue === 1 || parsedValue === 2) {
+            return {
+                player: parsedValue
+            };
+        }
+
+        return {
+            player: undefined
+        };
+    };
     describe('given defaults', () => {
         it('creates an in-memory repository', () => {
             const repository = new InMemoryRepository();
             expect(repository).toBeInstanceOf(InMemoryRepository);
+        });
+    });
+    describe('given a store', () => {
+        it('saves a board', () => {
+            const store = new Map();
+            const repository = new InMemoryRepository(store);
+            const asciiTable = `
+    |---|---|---|---|
+    |   |   |   |   |
+    |---|---|---|---|
+    |   |   |   |   |
+    |---|---|---|---|`;
+            const board = parseAsciiTable(asciiTable, customResolver);
+            const boardId = repository.save(board);
+            expect(store.get(boardId)).toBe(board);
         });
     });
 });
